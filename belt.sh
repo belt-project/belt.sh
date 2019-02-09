967375e46be8a85e552bb891620c9926deb3104c
Martin Rue <hello@martinrue.com>
2019-02-09 20:28:07 +0000

    Add EXIT trap for remote cleanup

--- belt.sh
+++ belt.sh
@@ -11,16 +11,16 @@ for file in "$BELT_LIB"/internal/*.sh; do source "$file"; done
 for file in "$BELT_LIB"/shims/*.sh; do source "$file"; done
 
 belt_begin_session() {
+	trap belt_cleanup_session EXIT
+
   _BELT_SSH_USER="$1"
   _BELT_SSH_HOST="$2"
   _BELT_SSH_PORT="${3:-22}"
 
-	local local_dir="$BELT_LIB/remote"
-	local remote_dir="$_BELT_SSH_USER@$_BELT_SSH_HOST:/tmp/belt"
-
-	scp -P "$_BELT_SSH_PORT" -r "$local_dir" "$remote_dir" &>/dev/null || belt_abort "library upload failed"
+	belt_cleanup_session
+	belt_remote_upload "$BELT_LIB/remote" "/tmp/belt"
 }
 
-belt_end_session() {
+belt_cleanup_session() {
 	belt_remote_exec "rm -rf /tmp/belt"
 }
