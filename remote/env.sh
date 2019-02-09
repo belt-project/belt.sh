bcba5f0b8147a2c14de59d39cc35e54610ca74b2
Tom Bell <tomb@tomb.io>
2019-02-09 19:38:17 +0000

    Update env.sh

--- remote/env.sh
+++ remote/env.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 export BELT_PATH="/tmp/belt"
+export BELT_TOOLS_PATH="$BELT_PATH/tools"
+
+export BELT_SYSTEMD_DIR="/etc/systemd/system"
 
 # shellcheck disable=SC1090
 for lib in "$BELT_PATH"/lib/*.sh; do source "$lib"; done
