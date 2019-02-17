a14f2531abf909decf3003219d6226d152ad931f
Tom Bell <tomb@tomb.io>
2019-02-17 19:11:03 +0000

    Add system_add_unit shim, and start/stop

--- local/shims/systemd.sh
+++ local/shims/systemd.sh
@@ -14,3 +14,22 @@ _systemd_internal() {
 systemd_get_logs() {
 	_systemd_internal "${FUNCNAME[0]}" "$@"
 }
+
+systemctl_start() {
+	_systemd_internal "${FUNCNAME[0]}" "$@"
+}
+
+systemctl_stop() {
+	_systemd_internal "${FUNCNAME[0]}" "$@"
+}
+
+systemd_add_unit() {
+	local unit="$1"
+
+	belt_remote_exec <<-SCRIPT
+		source /tmp/belt/env.sh
+		cp "$_BELT_ARCHIVE_EXTRACTED_PATH/$unit" "\$BELT_SYSTEMD_DIR/$_BELT_APP_NAME.service"
+		systemctl_reload
+		systemctl_enable "$_BELT_APP_NAME.service"
+	SCRIPT
+}
