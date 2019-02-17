5ca67a21e7515886cc2a717ce54a5bf38eeac10a
Martin Rue <hello@martinrue.com>
2019-02-17 18:01:39 +0000

    Add app_setup shim

---
+++ local/shims/app.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+app_setup() {
+	local path="$1"
+
+	belt_remote_exec <<-SCRIPT
+		source /tmp/belt/env.sh
+		mkdir -p "/app/$_BELT_APP_NAME"
+		cp "$_BELT_ARCHIVE_EXTRACTED_PATH/$path" "/app/$_BELT_APP_NAME"
+	SCRIPT
+}
