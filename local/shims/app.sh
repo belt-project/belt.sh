6def056e48f6a0e343bb0458c500b2c9c09ac662
Martin Rue <hello@martinrue.com>
2019-02-17 18:05:01 +0000

    Update app_setup to copy recursively

--- local/shims/app.sh
+++ local/shims/app.sh
@@ -6,6 +6,6 @@ app_setup() {
 	belt_remote_exec <<-SCRIPT
 		source /tmp/belt/env.sh
 		mkdir -p "/app/$_BELT_APP_NAME"
-		cp "$_BELT_ARCHIVE_EXTRACTED_PATH/$path" "/app/$_BELT_APP_NAME"
+		cp -R "$_BELT_ARCHIVE_EXTRACTED_PATH/$path" "/app/$_BELT_APP_NAME"
 	SCRIPT
 }
