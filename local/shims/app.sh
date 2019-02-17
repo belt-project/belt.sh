f2631458db03c00b1b87812564d6c5b6270e33e0
Tom Bell <tomb@tomb.io>
2019-02-17 18:46:56 +0000

    Add app_setup_permissions shim

--- local/shims/app.sh
+++ local/shims/app.sh
@@ -9,3 +9,13 @@ app_setup() {
 		cp -R "$_BELT_ARCHIVE_EXTRACTED_PATH/$path" "/app/$_BELT_APP_NAME"
 	SCRIPT
 }
+
+app_setup_permissions() {
+	local user="$1"
+
+	belt_remote_exec <<-SCRIPT
+		source /tmp/belt/env.sh
+		chown -R "$user" "/app/$_BELT_APP_NAME"
+		chmod -R 770 "/app/$_BELT_APP_NAME"
+	SCRIPT
+}
